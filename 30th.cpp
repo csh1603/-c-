62be73a18fb8c6ce1b70ef9c3d66b1676c36bcb2
csh1603 <91582838+csh1603@users.noreply.github.com>
2022-01-10 01:03:08 +0900

    Add files via upload

--- 30th.cpp
+++ 30th.cpp
@@ -3,7 +3,16 @@
 #include <stdio.h>
 
 int main(void) {
-	for (int i = 11; i > 0;i = i - 2) {
-
+	for (int i = 1; i < 7;i ++) {
+		for (int j = 13-2*i;j > 0;j--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = 1;i < 6;i++) {
+		for (int j = 1;j <= 1 + 2 * i;j++) {
+			printf("*");
+		}
+		printf("\n");
 	}
 }
